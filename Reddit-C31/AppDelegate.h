ea6cb2d340ad04abaa7becf24a1b6fbcfdd55875
jon <jdcorn@live.com>
2020-01-29 09:39:00 -0700

    Initial Commit

---
+++ Reddit-C31/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Reddit-C31
+//
+//  Created by Jon Corn on 1/29/20.
+//  Copyright © 2020 Jon Corn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
